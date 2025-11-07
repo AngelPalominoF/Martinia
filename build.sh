a7a2688d9004be58d1f6d9f0df15e0b0d6905e73
AngelPalominoF <angel_palomino1@usmp.pe>
2025-11-07 12:59:28 -0500

    subida2

--- build.sh
+++ build.sh
@@ -7,15 +7,5 @@ pip install uv
 echo "📦 Instalando dependencias con uv..."
 uv sync
 
-echo "⚙️ Construyendo el frontend..."
-cd src/frontend
-npm install
-npm run build
-cd ../../
-
-echo "📂 Copiando el build del frontend al backend..."
-mkdir -p src/backend/base/langflow/frontend
-cp -r src/frontend/dist/* src/backend/base/langflow/frontend/
-
 echo "✅ Build completado correctamente."
 
